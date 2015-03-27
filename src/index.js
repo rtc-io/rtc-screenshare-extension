80ce0534386c91fb200ccb735c1c112b6ec0e9d9
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-27 12:23:46 +1100

    Add the ability to cancel the screensharing request

--- src/index.js
+++ src/index.js
@@ -3,15 +3,32 @@ var extension = require('chromex')({
   manifest: require('./manifest.json')
 });
 
+// register the last request id
+var lastRequestId;
+
 // handle share events
 extension.on('share', function(opts, port, callback) {
   var targets = (opts || {}).targets || ['window', 'screen'];
 
-  chrome.desktopCapture.chooseDesktopMedia(targets, port.sender.tab, function(id) {
-    if (chrome.runtime.lastError) {
-      return callback(new Error(chrome.runtime.lastError.message));
-    }
+  // attempt to capture desktop media
+  lastRequestId = chrome.desktopCapture.chooseDesktopMedia(
+    targets,
+    port.sender.tab,
+    function(id) {
+      if (chrome.runtime.lastError) {
+        return callback(new Error(chrome.runtime.lastError.message));
+      }
 
-    callback(null, id);
-  });
+      callback(null, id, rid);
+    }
+  );
+});
+
+extension.on('cancel', function(opts, port, callback) {
+  if (! lastRequestId) {
+    return callback(new Error('no media has not been requested'));
+  }
+  
+  chrome.desktopCapture.cancelChooseDesktopMedia(lastRequestId);
+  callback();
 });
